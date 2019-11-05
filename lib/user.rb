aaaad56271a877e47d51913dbd1d1fcc57e1a787
Will Drougas <wdrougas@gmail.com>
2019-11-05 14:18:15 -0500

    Lab

---
+++ lib/user.rb
@@ -0,0 +1,21 @@
+class User
+
+    attr_accessor :first_name, :last_name
+
+    def initialize()
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    # def teach
+    #     KNOWLEDGE.sample
+    # end
+
+end
